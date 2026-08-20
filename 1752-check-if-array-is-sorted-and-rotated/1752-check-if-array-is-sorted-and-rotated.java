14fa76622e517f4443f8af30ce09863bc91adaa5
SUMIT RAYKA <raykasumit06@gmail.com>
2026-08-20 20:39:10 +0530

    Time: 0 ms (100%), Space: 43.1 MB (28.52%) - LeetHub

--- 1752-check-if-array-is-sorted-and-rotated/1752-check-if-array-is-sorted-and-rotated.java
+++ 1752-check-if-array-is-sorted-and-rotated/1752-check-if-array-is-sorted-and-rotated.java
@@ -1,20 +1,19 @@
 class Solution {
     public boolean check(int[] nums) {
-
         int count = 0;
         int n = nums.length;
-
-        for(int i=0;i<n;i++){
-
-            if(nums[i] > nums[(i+1)%n]){
+        
+        for (int i = 0; i < n; i++) {
+            // Compare current element with the next one (using modulo for wrap-around)
+            if (nums[i] > nums[(i + 1) % n]) {
                 count++;
             }
-
-            if(count>1){
+            // Optimization: exit early if we find more than one inversion
+            if (count > 1) {
                 return false;
             }
         }
-
+        
         return true;
     }
 }
